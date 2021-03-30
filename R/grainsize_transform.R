af250e08ffa292ad157011ec35e8a78bcdc1de38
Peter Kraska <peter.kraska@dfo-mpo.gc.ca>
2021-03-30 10:29:47 -0300

    updated grainsize transform with specific package calls for functions

--- R/grainsize_transform.R
+++ R/grainsize_transform.R
@@ -1,17 +1,18 @@
-#' grainsize_load
+#' grainsize_transform
 #'
 #' @param x .CSV file using the template included with this package
 #'
-#' @return
+#' @return returns a list with df_head and df_data for the header and data
+#'   information.
 #' @export
-#' @depends
-#' @import tidyverse, lubridate
-#' @examples
+#' @import dplyr readr tidyr lubridate
+#' @examples grainsize_transform(file)
 
 
 grainsize_transform <- function(x) {
-  raw <- read_csv("data/example.csv", col_names = FALSE) %>%
-    filter(X1 != is.na(X1))
+  raw <-
+    readr::read_csv(x, col_names = FALSE) %>%
+    dplyr::filter(X1 != is.na(X1))
 
   # pull project, region, and crew leader name from original file
   proj <- as.character(raw[1, 2])
@@ -58,43 +59,43 @@ grainsize_transform <- function(x) {
     df <- raw %>%
       t %>%
       as.data.frame(., stringsAsFactors = FALSE) %>%
-      slice(2:n()) %>%
-      filter(V4 != is.na(V4)) %>%   #filter out NAs introduced by the excel spreadsheet
-      rename_with(~ col_n) %>%  # Rename solumns using the vector of scraped column names you created earlier
-      remove_rownames() %>% #Remove rownames that correspond to matrix column names prior to being transposed
+      dplyr::slice(2:dplyr::n()) %>%
+      dplyr::filter(V4 != is.na(V4)) %>%   #filter out NAs introduced by the excel spreadsheet
+      dplyr::rename_with( ~ col_n) %>%  # Rename solumns using the vector of scraped column names you created earlier
+      tibble::remove_rownames() %>% #Remove rownames that correspond to matrix column names prior to being transposed
       replace(. == "N/A", NA) %>%
-      mutate(
+      dplyr::mutate(
         project = proj,
         # Set project variables and format Datetime correctly from df, convert
         # to seconds since 1900, then combine with 'IDs', 'Core #', and 'Filter
         # #' for unique id (`uid`)
         region = reg,
         leader = lead,
-        datetime = dmy_hms(paste(date, `time (gmt)`, sep = " ")),
+        datetime = lubridate::dmy_hms(paste(date, `time (gmt)`, sep = " ")),
         uid = paste(as.character(datetime),
                     ids,
                     `core #`,
                     `filter #`,
                     sep = "_")
       ) %>%
-      select(uid,
-             project,
-             region,
-             leader,
-             lat,
-             long,
-             datetime,
-             everything()) %>%
-      select(-date, -`time (gmt)`, -diameter)
+      dplyr::select(uid,
+                    project,
+                    region,
+                    leader,
+                    lat,
+                    long,
+                    datetime,
+                    everything()) %>%
+      dplyr::select(-date,-`time (gmt)`,-diameter)
 
     df_head <- df %>%
-      select(1:23) %>%
-      glimpse
+      dplyr::select(1:23)
 
     df_data <- df %>%
-      select(uid, 24:ncol(.)) %>%
-      pivot_longer(-uid, names_to = "diameter", values_to = "measurement") %>%
-      mutate(measurement = as.numeric(measurement)) %>%
-      glimpse
+      dplyr::select(uid, 24:ncol(.)) %>%
+      tidyr::pivot_longer(-uid, names_to = "diameter", values_to = "measurement") %>%
+      dplyr::mutate(measurement = as.numeric(measurement))
+
+    list(header = df_head, data = df_data)
   }
 }
